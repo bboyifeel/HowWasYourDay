74f1160733fd9ad8ad27a4fe52d8807e03ad222f
Igor <bboyifeel@gmail.com>
2020-04-10 21:09:15 +0200

    register and login works

--- app/src/main/java/com/lu/uni/igorzfeel/howwasyourday/LoginActivity.kt
+++ app/src/main/java/com/lu/uni/igorzfeel/howwasyourday/LoginActivity.kt
@@ -1,10 +1,10 @@
 package com.lu.uni.igorzfeel.howwasyourday
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
@@ -14,18 +14,42 @@ class LoginActivity: AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         login_button_login.setOnClickListener {
-            val email = email_edittext_login.text.toString()
-            val password = password_edittext_login.text.toString()
-
-            Log.d("LoginActivity", "Email is: $email")
-            Log.d("LoginActivity", "Password is: $password")
+            performLogin()
         }
 
         register_textview_login.setOnClickListener{
-//            val intent = Intent(this, RegisterActivity::class.java)
-//            startActivity(intent)
             finish()
         }
+    }
+
+    private fun performLogin() {
+        val email = email_edittext_login.text.toString()
+        val password = password_edittext_login.text.toString()
+
+        Log.d("LoginActivity", "Email is: $email")
+        Log.d("LoginActivity", "Password is: $password")
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this
+                , "Email or password cannot be empty"
+                , Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("LoginActivity", "uid: ${it.result?.user?.uid}")
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this
+                    , "Failed to login user: ${it.message}"
+                    , Toast.LENGTH_SHORT)
+                    .show()
+            }
     }
 
 }
