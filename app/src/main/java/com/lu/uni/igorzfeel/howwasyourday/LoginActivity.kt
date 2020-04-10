e4c055ff29d2513f3d2229548e2efce6f122ecee
Igor <bboyifeel@gmail.com>
2020-04-10 20:21:00 +0200

    *

--- app/src/main/java/com/lu/uni/igorzfeel/howwasyourday/LoginActivity.kt
+++ app/src/main/java/com/lu/uni/igorzfeel/howwasyourday/LoginActivity.kt
@@ -1,14 +1,31 @@
 package com.lu.uni.igorzfeel.howwasyourday
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("LoginActivity", "Email is: $email")
+            Log.d("LoginActivity", "Password is: $password")
+        }
+
+        register_textview_login.setOnClickListener{
+//            val intent = Intent(this, RegisterActivity::class.java)
+//            startActivity(intent)
+            finish()
+        }
     }
 
 }
